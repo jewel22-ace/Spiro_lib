f323bbdedddd3caf631acfc5d7afbbf692621e79
Mosaif Ali <mosaif.ali.39@github.com>
2023-05-11 13:57:10 +0530

    PCA Analysis

--- analysis.py
+++ analysis.py
@@ -11,43 +11,21 @@ from sklearn import preprocessing
 from sklearn import utils
 dataset = pd.read_csv('features_data_1.csv')
 X = dataset.iloc[:, :-1].values
-print(X)
+
 y = dataset.iloc[:, -1].values
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=0)
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-# X_train = X_train.fillna(X_train.mean())
-# X_test = X_test.fillna(X_train.mean())
-# X_train = X_train[~np.isnan(X_train)]
-# X_test = X_test[~np.isnan(X_test)]
-# X_train = X_train.reshape(-1, 1)
-# X_test = X_test.reshape(-1, 1)
+
+scaler = StandardScaler()
+scaler.fit(X)
+scaled_data = scaler.transform(X)
+print(scaled_data)
 pca = PCA(n_components=2)
-X_train = pca.fit_transform(X_train)
-X_test = pca.transform(X_test)
-lab = preprocessing.LabelEncoder()
-y_test = lab.fit_transform(y_test)
-y_train = lab.fit_transform(y_train)
-classifier = LogisticRegression(random_state=0)
-classifier.fit(X_train, y_train)
-y_pred = classifier.predict(X_test)
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
-accuracy_score(y_test, y_pred)
-X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
-                     np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha=0.75, cmap=ListedColormap(('red', 'green', 'blue')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c=ListedColormap(('red', 'green', 'blue'))(i), label=j)
-plt.title('Logistic Regression (Test set)')
-plt.xlabel('PC1')
-plt.ylabel('PC2')
-plt.legend()
+pca.fit(scaled_data)
+x_pca = pca.transform(scaled_data)
+print(scaled_data.shape)
+print(x_pca.shape)
+print(pca.explained_variance_ratio_)
+plt.figure(figsize=(8, 6))
+plt.scatter(x_pca[:, 0], x_pca[:, 1], c=y)
+plt.xlabel('First principle component')
+plt.ylabel('Second principle component')
 plt.show()
